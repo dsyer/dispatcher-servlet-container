d49493d78b49e3b38d0f0167c182d294f86dddb7
Dave Syer <dsyer@pivotal.io>
2019-12-20 13:33:35 +0000

    Add jafu sample

---
+++ jafu/compile.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+ARTIFACT=jafu-dispatcher
+MAINCLASS=com.example.jafu.JafuApplication
+VERSION=0.0.1-SNAPSHOT
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+cd `dirname $0`
+
+rm -rf target
+mkdir -p target/native-image
+
+echo "Packaging $ARTIFACT with Maven"
+../mvnw -DskipTests package > target/native-image/output.txt
+
+JAR="$ARTIFACT-$VERSION.jar"
+rm -f $ARTIFACT
+echo "Unpacking $JAR"
+cd target/native-image
+jar -xvf ../$JAR >/dev/null 2>&1
+cp -R META-INF BOOT-INF/classes
+
+LIBPATH=`find BOOT-INF/lib | tr '\n' ':'`
+CP=BOOT-INF/classes:$LIBPATH
+
+GRAALVM_VERSION=`native-image --version`
+echo "Compiling $ARTIFACT with $GRAALVM_VERSION"
+time native-image \
+  --no-server \
+  --no-fallback \
+  -H:+JNI \
+  -H:EnableURLProtocols=http,https,jar \
+  --initialize-at-build-time \
+  --initialize-at-run-time=org.springframework.core.io.VfsUtils \
+  -H:ReflectionConfigurationFiles=../../tomcat-reflection.json,../../reflection-config.json -H:ResourceConfigurationFiles=../../tomcat-resource.json -H:JNIConfigurationFiles=../../tomcat-jni.json \
+  --enable-https \
+  -Dsun.rmi.transport.tcp.maxConnectionThreads=0 \
+  -H:Name=$ARTIFACT \
+  -H:+ReportExceptionStackTraces \
+  --allow-incomplete-classpath \
+  --report-unsupported-elements-at-runtime \
+  -cp $CP $MAINCLASS >> output.txt
+
+if [[ -f $ARTIFACT ]]
+then
+  printf "${GREEN}SUCCESS${NC}\n"
+  mv ./$ARTIFACT ..
+  exit 0
+else
+  printf "${RED}FAILURE${NC}: an error occurred when compiling the native-image.\n"
+  exit 1
+fi
+
